0c84e19708243b6cbe4de48dbbfb5f6f9335e8f8
PavelShevchenko <funakoshi@inbox.ru>
2022-09-27 15:30:52 +0300

    rename variables

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,18 +1,18 @@
 public class MyFirstGame {
     public static void main (String[] args) {
-        int numSecret = 22;
-        int numLeft = 1;
-        int numRight = 100;
-        int numPlayer = 50;
-        while (numPlayer != numSecret) {
-            if (numPlayer > numSecret) {
-                System.out.println("число " + numPlayer + " больше того, что загадал компьютер");
-                numRight = numPlayer;
+        int Secretnum = 22;
+        int startRange = 1;
+        int endRange = 100;
+        int Playernum = 50;
+        while (Playernum != Secretnum) {
+            if (Playernum > Secretnum) {
+                System.out.println("число " + Playernum + " больше того, что загадал компьютер");
+                endRange = Playernum;
             } else {
-                System.out.println("число " + numPlayer + " меньше того, что загадал компьютер");
-                numLeft = numPlayer;
+                System.out.println("число " + Playernum + " меньше того, что загадал компьютер");
+                startRange = Playernum;
             }
-            numPlayer = (numRight + numLeft) / 2;
+            Playernum = (endRange + startRange) / 2;
         }
         System.out.println("Вы победили!");
     }
