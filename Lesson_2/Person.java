b92e68829747ee03c66911d0ec8e76be5bf1434c
PavelShevchenko <funakoshi@inbox.ru>
2022-09-28 10:17:35 +0300

    start Lesson 2

---
+++ Lesson_2/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+
+    string humanGender = "men";
+    String humanName = "Jeeves"
+    int age = 37;
+    double height = 1.90;
+    double weight = 82.5;
+
+    void move() {
+        System.out.println("иду");
+    }
+
+    void sit() {
+        System.out.println("сижу");
+    }
+
+    void run() {
+        System.out.println("бегу");
+    }
+
+    string speak() {
+        return "Текст"
+    }
+
+    string learnJava() {
+        return "ответ на задание"
+    }
+
+}
