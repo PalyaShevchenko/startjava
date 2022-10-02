abdebf20157ed1b92e5c5e7be5ab2c7a9f5c3992
PavelShevchenko <funakoshi@inbox.ru>
2022-10-02 19:04:56 +0300

    Lesson 2 fin DZ Calc правка

--- Lesson_2/CalculatorTest.java
+++ Lesson_2/CalculatorTest.java
@@ -4,23 +4,24 @@ public class CalculatorTest {
 
     public static void main (String[] args) {
         Calculator calc = new Calculator();
-        Scanner scannerNum = new Scanner(System.in);
-        Scanner scannerLine = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         String userReply;
         do {
             System.out.print("Введите первое число: ");
-            int num1 = scannerNum.nextInt();
+            int num1 = scanner.nextInt();
             calc.setNum1(num1);
             System.out.print("Введите знак математической операции: ");
-            String sign = scannerLine.nextLine();
+            scanner.nextLine();
+            String sign = scanner.nextLine();
             calc.setSign(sign);
             System.out.print("Введите второе число: ");
-            int num2 = scannerNum.nextInt();
+            int num2 = scanner.nextInt();
             calc.setNum2(num2);
             System.out.print("результат " + calc.getResult());
             do {
                 System.out.print("\n\nХотите продолжить вычисления? [yes/no]:");
-                userReply = scannerLine.nextLine();
+                scanner.nextLine();
+                userReply = scanner.nextLine();
             } while ((!userReply.equalsIgnoreCase("yes")) && (!userReply.equalsIgnoreCase("no")));
         } while (userReply.equalsIgnoreCase("yes"));
     }
