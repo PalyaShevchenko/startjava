6ce990b89b4f08a77eb0d46c5f4fb03e609034ed
PavelShevchenko <funakoshi@inbox.ru>
2022-10-03 17:24:18 +0300

    Lesson 2 fin DZ добавлены Егеря

--- Lesson_2/JaegerTest.java
+++ Lesson_2/JaegerTest.java
@@ -1,11 +1,25 @@
 public class JaegerTest {
     
     public static void main (String[] args) {
-        Jaeger Jaeger1 = new Jaeger();
-        Jaeger1.setModelName("Gipsy Avenger");
-        Jaeger1.setMark("Mark-6");
-        Jaeger1.setKaijuKilled(1);
-        Jaeger Jaeger2 = new Jaeger("Cherno Alpha","Mark-1",6);
-        
+        Jaeger jaegerOne = new Jaeger();
+        jaegerOne.setModelName("Gipsy Avenger");
+        jaegerOne.setMark("Mark-6");
+        jaegerOne.setKaijuKilled(1);
+        System.out.println("Имя робота: " + jaegerOne.getModelName());
+        System.out.println("Марка робота: " + jaegerOne.getMark());
+        System.out.println("Число унечтоженных Кайдзю: " + jaegerOne.getKaijuKilled());
+        System.out.println("поиск Кайдзю: " + jaegerOne.scanKaiju() +"\n");
+        System.out.println("Вооружение к бою");
+        jaegerOne.useVortexCannon();
+        System.out.println("\nКайдзю уничтожен\n");
+        jaegerOne.setKaijuKilled(jaegerOne.getKaijuKilled() + 1);
+        System.out.println("Имя робота: " + jaegerOne.getModelName());
+        System.out.println("Число унечтоженных Кайдзю: " + jaegerOne.getKaijuKilled() + "\n");
+
+        Jaeger jaegerTwo = new Jaeger("Cherno Alpha","Mark-1",6);
+        System.out.println("Имя робота: " + jaegerTwo.getModelName());
+        System.out.println("Марка робота: " + jaegerTwo.getMark());
+        System.out.println("Число унечтоженных Кайдзю: " + jaegerTwo.getKaijuKilled());
+        jaegerTwo.move();
     }
 }
