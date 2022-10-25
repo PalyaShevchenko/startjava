a905058559ffc1002e8402bd2fd035629baeed72
PavelShevchenko <funakoshi@inbox.ru>
2022-10-25 13:22:17 +0500

    Итоговое ДЗ урок4 Калькулятор со звездочкой

--- src/com/startjava/lesson_2_3_4/calculator/Calculator.java
+++ src/com/startjava/lesson_2_3_4/calculator/Calculator.java
@@ -1,38 +1,25 @@
 package com.startjava.lesson_2_3_4.calculator;
 
+import java.util.InputMismatchException;
+
 public class Calculator {
 
-    private String expression;
-    private int num1;
-    private int num2;
-    private String sign;
-
-    public void setExpression(String expression) {
-        this.expression = expression;
-        String[] mathExpressionArr = expression.split(" ");
-        num1 = Integer.parseInt(mathExpressionArr[0]);
-        sign = mathExpressionArr[1];
-        num2 = Integer.parseInt(mathExpressionArr[2]);
-    }
-
-    public long сalc() {
-
-        switch (sign) {
-            case "+" :
-                return Math.addExact(num1, num2);
-            case "-" :
-                return Math.subtractExact(num1, num2);
-            case "*" :
-                return Math.multiplyExact(num1, num2);
-            case "/" :
-                return Math.floorDiv(num1, num2);
-            case "^" :
-                return (long) Math.pow(num1,num2);
-            case "%" :
-                return Math.floorMod(num1, num2);
-            default:
-                System.out.println("неизвестный оператор");
+    public static long calc(String expression) {
+        String[] partsExpression = expression.split(" ");
+        int num1 = Integer.parseInt(partsExpression[0]);
+        String mathOperator = partsExpression[1];
+        int num2 = Integer.parseInt(partsExpression[2]);
+        if ((num1 < 1) || (num2 < 1)) {
+            throw new InputMismatchException();
         }
-        return 0;
+        return switch (mathOperator) {
+            case "+" -> Math.addExact(num1, num2);
+            case "-" -> Math.subtractExact(num1, num2);
+            case "*" -> Math.multiplyExact(num1, num2);
+            case "/" -> Math.floorDiv(num1, num2);
+            case "^" -> (long) Math.pow(num1, num2);
+            case "%" -> Math.floorMod(num1, num2);
+            default -> throw new IllegalStateException(); //"неизвестный оператор"
+        };
     }
 }
--- src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_2_3_4/calculator/CalculatorTest.java
@@ -1,19 +1,26 @@
 package com.startjava.lesson_2_3_4.calculator;
 
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class CalculatorTest {
 
     public static void main (String[] args) {
-        Calculator calc = new Calculator();
         Scanner console = new Scanner(System.in);
         String userAnswer;
         do {
             System.out.print("Введите математическое выражение: ");
-            calc.setExpression(console.nextLine());
-            //console.nextLine();
-            System.out.print("результат " + calc.сalc());
-            console.nextLine();
+            try {
+                System.out.print("результат " + Calculator.calc(console.nextLine()));
+            } catch (NumberFormatException exception) {
+                System.out.print("Число должно быть целым");
+            } catch (ArithmeticException exception) {
+                System.out.print("Деление на 0");
+            } catch (IllegalStateException exception) {
+                System.out.print("Некорректный знак операции");
+            } catch (InputMismatchException exception) {
+                System.out.print("Число должно быть больше 0");
+            }
             do {
                 System.out.print("\n\nХотите продолжить вычисления? [yes/no]:");
                 userAnswer = console.nextLine().toLowerCase();
