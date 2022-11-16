9559e4cf5017a7d331dc388f01b20456a407411a
PavelShevchenko <funakoshi@inbox.ru>
2022-11-16 15:45:06 +0500

    Урок 5 введение в psql

---
+++ src/com/startjava/lesson_5/init_db.sql
@@ -0,0 +1,12 @@
+INSERT INTO Jaegers (modelName,     mark,   height,  weight,        status,      origin, launch, kaijuKill)
+VALUES              ('Gipsy Danger',     3,    79.25,  1.98,   'Destroyed',       'USA',   2017,     9),
+                    ('Striker Eureka',   5,     76.2,  1.85,   'Destroyed', 'Australia',   2019,    11),
+                    ('Cherno Alpha',     1,    85.34, 2.412,   'Destroyed',    'Russia',   2015,     6),
+                    ('Crimson Typhoon',  4,     76.2, 1.722,   'Destroyed',     'China',   2018,     7),
+                    ('Brawler Yukon',    1,       79,   2.1,   'Destroyed',       'USA',   2015,     2),
+                    ('Coyote Tango',     1,    85.34, 2.312,   'Destroyed',     'Japan',   2015,     2),
+                    ('Shaolin Rogue',    3,       73,   1.8,   'Destroyed',     'China',   2017,     2),
+                    ('Tacit Ronin',      1,    74.37,  7.45,   'Destroyed',     'Japan',   2015,     3),
+                    ('Diablo Intercept', 2,    70.73,  7.05,   'Destroyed',     'Chile',   2016,     3),
+                    ('Guardian Bravo',   6,    73.21, 1.975,      'Active',    'Russia',   2033,     1);
+ 
---
+++ src/com/startjava/lesson_5/queries.sql
@@ -0,0 +1,52 @@
+--выведите всю таблицу
+SELECT * 
+  FROM Jaegers;
+
+--отобразите только не уничтоженных роботов
+SELECT * 
+  FROM Jaegers 
+ WHERE status = 'Active';
+
+--отобразите роботов нескольких серий: Mark-1 и Mark-4
+SELECT * 
+  FROM Jaegers 
+ WHERE mark IN (1, 4);
+
+--отобразите всех роботов, кроме Mark-1 и Mark-4
+SELECT * 
+  FROM Jaegers  
+ WHERE mark NOT IN (1, 4);
+
+--отсортируйте таблицу по убыванию по столбцу mark
+SELECT * 
+  FROM Jaegers 
+ ORDER BY mark DESC;
+
+--отобразите информацию о самом старом роботе
+SELECT * 
+  FROM Jaegers 
+  WHERE launch = 
+       (SELECT MIN(launch) 
+          FROM Jaegers);
+
+--отобразите роботов, которые уничтожили больше всех kaiju
+SELECT * 
+  FROM Jaegers 
+ WHERE kaijuKill = 
+         (SELECT MAX(kaijuKill) 
+            FROM Jaegers);
+
+--отобразите средний вес роботов
+SELECT AVG(weight) 
+  FROM Jaegers; 
+
+--увеличьте на единицу количество уничтоженных kaiju у роботов, которые до сих пор не разрушены
+UPDATE Jaegers 
+   SET kaijuKill = kaijuKill + 1 
+ WHERE status = 'Active';
+
+--удалите уничтоженных роботов
+DELETE FROM Jaegers 
+ WHERE status = 'Destroyed';
+
+
